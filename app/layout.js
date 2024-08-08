97b113c1afafbd4255ce59d104ae7fc621de95fe
Saif <muhammadsaif713@gmail.com>
2024-08-09 02:16:55 +0400

    feat: Add sign-in and sign-up pages and functionality

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,10 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +18,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+        <body className={inter.className}>
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
